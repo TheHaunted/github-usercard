601d690c79da9a146128f2e682977c256e458f56
TheHaunted <kevin_lopez@live.com>
2019-07-11 17:40:53 -0500

    added instructor cards for MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -30,7 +30,16 @@ axios.get('https://api.github.com/users/thehaunted')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach(user => {
+  axios.get(`https://api.github.com/users/${user}`)
+    .then(resp => {
+      console.log(resp.data);
+      let user = resp.data;
+      createCard(user);
+    })
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
